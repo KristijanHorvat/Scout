dd0991f409c7ffa10d3a72f17cb0fe5849a5618d
KristijanHorvat <92637883+KristijanHorvat@users.noreply.github.com>
2023-01-16 16:54:14 +0100

    commit 16/1/2023

---
+++ app/src/main/java/com/example/scoutv1/Item.kt
@@ -0,0 +1,8 @@
+package com.example.scoutv1
+
+data class Item(
+    var name: String,
+    var imageLink: String,
+    var description: String,
+    var phoneNumber: String
+)
--- app/src/main/java/com/example/scoutv1/addItemActivity.kt
+++ app/src/main/java/com/example/scoutv1/addItemActivity.kt
@@ -3,6 +3,7 @@ package com.example.scoutv1
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Activity.RESULT_OK
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -13,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.core.app.ActivityCompat
@@ -37,51 +39,78 @@ class addItemActivity : AppCompatActivity() {
     var storageReference: StorageReference? = null
     lateinit var btn_upload_image: Button
     lateinit var imagePreview: ImageView
+    lateinit var itemName: EditText
+    lateinit var itemDescription: EditText
+    lateinit var itemPhoneNumber: EditText
     var path: String = ""
-    val db = Firebase.firestore
+    private val db = Firebase.firestore
     lateinit var categoriesSpinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
 
-        btn_upload_image = findViewById(R.id.button9)
+
         firebaseStore = FirebaseStorage.getInstance()
         storageReference = FirebaseStorage.getInstance().reference
 
+        btn_upload_image = findViewById(R.id.button9)
         imagePreview = findViewById<ImageView>(R.id.imageView)
-
-        imagePreview.setOnClickListener { launchGallery() }
-        btn_upload_image.setOnClickListener {
-            uploadImage()
-
-        }
+        itemName = findViewById(R.id.ItemName)
+        itemDescription = findViewById(R.id.ItemDescription)
+        itemPhoneNumber = findViewById(R.id.ItemPhoneNUmber)
         categoriesSpinner = findViewById(R.id.itemCategory)
-// Create an ArrayAdapter using the string array and a default spinner layout
+
+        //Spinner adapter
         ArrayAdapter.createFromResource(
             this,
             R.array.categories_array,
             android.R.layout.simple_spinner_item
         ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
             categoriesSpinner.adapter = adapter
         }
+
+
+        imagePreview.setOnClickListener { launchGallery() }
+        btn_upload_image.setOnClickListener {
+            uploadImage()
+
+            val item = Item(
+                itemName.text.toString(),
+                path.toString(),
+                itemDescription.text.toString(),
+                itemPhoneNumber.text.toString()
+            )
+            var itemSelected = categoriesSpinner.getSelectedItem().toString();
+            db.collection(itemSelected)
+                .add(item)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                    Toast.makeText(this, "Upload success", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                    Toast.makeText(this, "Error adding item!", Toast.LENGTH_SHORT).show()
+                }
+        }
+
     }
+
     private fun launchGallery() {
         val intent = Intent()
         intent.type = "image/*"
         intent.action = Intent.ACTION_GET_CONTENT
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
     }
-    private fun uploadImage(){
-        if(filePath != null){
+
+    private fun uploadImage() {
+        if (filePath != null) {
             path = "myImages/" + UUID.randomUUID().toString()
             val ref = storageReference?.child(path)
             val uploadTask = ref?.putFile(filePath!!)
-            Toast.makeText(this, "Upload success", Toast.LENGTH_SHORT).show()
-        }else{
+            Toast.makeText(this, "Upload image success", Toast.LENGTH_SHORT).show()
+        } else {
             Toast.makeText(this, "Please Upload an Image", Toast.LENGTH_SHORT).show()
         }
     }
@@ -89,10 +118,9 @@ class addItemActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
-            if(data == null || data.data == null){
+            if (data == null || data.data == null) {
                 return
             }
-
             filePath = data.data
             try {
                 val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)

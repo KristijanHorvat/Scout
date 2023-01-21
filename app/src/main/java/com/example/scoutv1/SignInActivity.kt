fa8329a2f22441a2335c35230e5057d59459dd2c
KristijanHorvat <92637883+KristijanHorvat@users.noreply.github.com>
2023-01-22 00:54:40 +0100

    +R.id.... on SignInActivity.kt SignUpActivity.kt without binding for project purposes

--- app/src/main/java/com/example/scoutv1/SignInActivity.kt
+++ app/src/main/java/com/example/scoutv1/SignInActivity.kt
@@ -3,6 +3,8 @@ package com.example.scoutv1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.example.scoutv1.databinding.ActivitySignInBinding
@@ -11,22 +13,25 @@ import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignInBinding
+    //private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignInBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_sign_in)
+
+        val signUpTextView = findViewById<TextView>(R.id.textView)
+        val resetPassTextView = findViewById<TextView>(R.id.resetPass)
+        val emailField = findViewById<EditText>(R.id.emailEt)
+        val passField = findViewById<EditText>(R.id.passET)
+        val signInButton = findViewById<Button>(R.id.button)
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.textView.setOnClickListener {
+        signUpTextView.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
-
-        val resetPassTextView = findViewById<TextView>(R.id.resetPass)
 
         resetPassTextView.setOnClickListener{
             firebaseAuth!!.sendPasswordResetEmail(emailEt.text.toString())
@@ -39,9 +44,9 @@ class SignInActivity : AppCompatActivity() {
                 }
         }
 
-        binding.button.setOnClickListener {
-            val email = binding.emailEt.text.toString()
-            val pass = binding.passET.text.toString()
+        signInButton.setOnClickListener {
+            val email = emailField.text.toString()
+            val pass = passField.text.toString()
 
             if (email.isNotEmpty() && pass.isNotEmpty()) {
 
--- app/src/main/java/com/example/scoutv1/SignUpActivity.kt
+++ app/src/main/java/com/example/scoutv1/SignUpActivity.kt
@@ -3,6 +3,9 @@ package com.example.scoutv1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.example.scoutv1.databinding.ActivitySignInBinding
 import com.example.scoutv1.databinding.ActivitySignUpBinding
@@ -10,24 +13,29 @@ import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignUpBinding
+    //private lateinit var binding: ActivitySignUpBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_sign_up)
+
+        val emailField = findViewById<EditText>(R.id.emailEt)
+        val passField = findViewById<EditText>(R.id.passET)
+        val confirmPassField = findViewById<EditText>(R.id.confirmPassEt)
+        val signInScreenTextView = findViewById<TextView>(R.id.textView)
+        val signUpButton = findViewById<Button>(R.id.button)
 
         firebaseAuth = FirebaseAuth.getInstance()
 
-        binding.textView.setOnClickListener {
+        signInScreenTextView.setOnClickListener {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
-        binding.button.setOnClickListener {
-            val email = binding.emailEt.text.toString()
-            val pass = binding.passET.text.toString()
-            val confirmPass = binding.confirmPassEt.text.toString()
+        signUpButton.setOnClickListener {
+            val email = emailField.text.toString()
+            val pass = passField.text.toString()
+            val confirmPass = confirmPassField.text.toString()
 
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
                 if (pass.equals(confirmPass)) {
